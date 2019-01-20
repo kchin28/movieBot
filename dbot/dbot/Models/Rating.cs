1aa8879aaa1a50dbde9ee078db3cfee594d3e385
Ryan Huard <ryan.huard@live.ca>
2019-01-19 20:11:15 -0700

    Add OMDB models

---
+++ dbot/dbot/Models/Rating.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+
+namespace dbot.Models
+{
+    class Rating
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+}
