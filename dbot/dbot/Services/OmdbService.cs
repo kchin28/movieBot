1e55cd1b6d0ab37d9be8e729cb9c34e8924fe4ef
Ryan Huard <ryan.huard@live.ca>
2019-01-19 20:53:55 -0700

    Finish baseline omdbservice, with no error handling because that's over rated

--- dbot/dbot/Services/OmdbService.cs
+++ dbot/dbot/Services/OmdbService.cs
@@ -1,11 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using RestSharp;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using dbot.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace dbot.Services
 {
     class OmdbService
     {
+        private string _omdbToken;
+        private string _baseUri;
+        private string _posterBaseUri;
+
+        public OmdbService(string omdbToken)
+        {
+            _omdbToken = omdbToken;
+            _baseUri = $"http://www.omdbapi.com/?apikey={_omdbToken}&";
+            _posterBaseUri = $"http://www.img.omdbapi.com/?apikey={_omdbToken}&";
+        }
+
+        private async Task<T> Request<T>(string query)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"{_baseUri}{query}").ConfigureAwait(false);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    return (T)JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings());
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        
+        public async Task<Movie> GetMovieByTitle(string title)
+        {
+            var movie = await Request<Movie>($"t={title}").ConfigureAwait(false);
+            return movie;
+        }
+
+        public async Task<Movie> GetItemByID(string id)
+        {
+            var movie = await Request<Movie>($"i={id}").ConfigureAwait(false);
+            return movie;
+        }
     }
 }
